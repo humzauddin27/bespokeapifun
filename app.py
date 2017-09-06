ebee19ae0a84c43d4f4f9d5694472be30f0a653c
Humza Faheemuddin <humzauddin7@hotmail.com>
2017-09-06 16:19:30 -0400

    Updated app.py to have a working POST/DELETE/GET

--- app.py
+++ app.py
@@ -6,55 +6,71 @@ api = Api(app)
 
 chelseafc = {
 	'Goalkeepers': {
-				'13':'Thiabut Courtois'
+				13:'Thiabut Courtois'
 				},
 	'Defenders': {
-				'3':'Marcos Alonso',
-				'28':'Cesar Azpilicueta',
-				'24':'Gary Cahill',
-				'30':'David Luiz',
+				3:'Marcos Alonso',
+				28:'Cesar Azpilicueta',
+				24:'Gary Cahill',
+				30:'David Luiz',
 				},
 	'Midfielders': {
-				'7':'N`golo Kante',
-				'10':'Eden Hazard',
-				'4':'Cesc Fabregas',
-				'11':'Pedro Rodriguez',
-				'22':'Willian'
+				7:'N`golo Kante',
+				10:'Eden Hazard',
+				4:'Cesc Fabregas',
+				11:'Pedro Rodriguez',
+				22:'Willian'
 				},
 	'Strikers': {
-				'9':'Alvaro Morata',
-				'19':'Diego Costa',
-				'23':'Michy Batshuayi'
+				9:'Alvaro Morata',
+				19:'Diego Costa',
+				23:'Michy Batshuayi'
 				}
 }
+
+parser = reqparse.RequestParser()
+parser.add_argument('num')
+parser.add_argument('pos')
+parser.add_argument('name')
 
 def errorMessage(kit_number):
 	if kit_number not in chelseafc:
 		abort(404, message="Whoops, {} doesn't exist!".format(kit_number))
 
 
-class player(Resource):
-    	def get(self, kit_number):
-    		errorMessage(kit_number)
-        	return {kit_number: chelseafc[kit_number]}
+class Player(Resource):
+    	def get(self, position, kit_number):
+        	return {kit_number:chelseafc[position][kit_number]}
 
-        def put(self, kit_number):
-        	chelseafc[kit_number] = request.form['data']
-        	return {kit_number: chelseafc[kit_number]}
+        def put(self, position, kit_number):
+        	chelseafc[position][kit_number] = request.form['data']
+        	return {kit_number:chelseafc[position][kit_number]}
 
-        ##def post(self, id):
+        def delete(self, position, kit_number):
+        	temp = chelseafc[position][kit_number]
+        	del chelseafc[position][kit_number]
+        	return 'Deleted ' + temp
 
-        ##def delete(self, id):
+class Position(Resource):
+	def get(self, position):
+		return chelseafc[position]
 
-class squadList(Resource):
+class Squad(Resource):
 	def get(self):
 		return chelseafc
 
-	#def post
+	def post(self):
+		args = parser.parse_args()
+		if (int(args['num']) > 100):
+			return "Can't make a players number so high!", 404
+		else:
+			chelseafc[args['pos']][args['num']] = args['name']
+			return {args['num']:args['name']}, 200
 
 
-api.add_resource(squadList, '/chelseafc')
-api.add_resource(player, '/chelseafc/<string:kit_number>')
+api.add_resource(Squad, '/chelseafc')
+api.add_resource(Position, '/chelseafc/<string:position>')
+api.add_resource(Player, '/chelseafc/<string:position>/<string:kit_number>')
 
 if __name__ == '__main__':
     app.run(debug=True)
